f4d6be603c953fef751bf4bf0c314a96fa36c305
devlos0322 <devlos0322@gmail.com>
2022-03-11 14:09:41 +0900

    Initial commit

---
+++ src/main/java/org/winitech/practiceelasticspringboot/PracticeElasticSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.winitech.practiceelasticspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeElasticSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeElasticSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/winitech/practiceelasticspringboot/PracticeElasticSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.winitech.practiceelasticspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeElasticSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
